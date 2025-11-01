e53b5859eec25bf9d0277896f066b0cd61fb4ea6
xdilobar2197 <xdilobar2197@gmail.com>
2025-11-01 17:25:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#1
+son = (22, 1, 4, 8)
+yigindi =sum(son)
+print(yigindi)
+
+
+#2
+royhat = (1, 44, 2, 9, 6, 66)
+son = max(royhat)
+print(son)
+
+
+#3
+son1 = (1, 2, 3)
+son2 = (4, 5, 6)
+royhat = son1 + son2
+print(royhat)
+
+
+#4
+royhat = (11, 22, 33, 44, 8, 6)
+
+juft_son = ()
+for son in royhat:
+    if son % 2 == 0:
+        juft_son += (son,)
+
+print(juft_son)
+
+
+#5
+royhat = (1, 2, 3, 4, 5)
+son = royhat[::-1]
+print(son)
+
+
